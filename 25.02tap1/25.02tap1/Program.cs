a8599335a3feccb63ebe1ae0563a27759b001baa
tural10 <turalja@code.edu.az>
2023-02-25 10:40:39 +0400

    first commit

---
+++ 25.02tap1/25.02tap1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _25._02tap1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("reqemi daxil edin:");
+            l1:
+            int.TryParse(Console.ReadLine(), out int num);
+            int uzunluq = (int)Math.Log10(num) + 1;
+
+            if (uzunluq == 4) 
+            {
+                int a = 7 *(int)Math.Pow(10, uzunluq + 1);
+                num *= 10;
+                int result = a + num + 8;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.Write("reqemi yeniden daxil edin:");
+                goto l1;
+            }
+
+
+        }
+    }
+}
